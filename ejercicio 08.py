cf9a91137ab31c286e29ccff096962129c156885
Florencia Morisse <florenciamorisse@gmail.com>
2022-08-11 11:19:00 -0300

    ejercicio 8 de python

---
+++ ejercicio 08.py
@@ -0,0 +1,17 @@
+# 8) Crear una tupla en Python con el nombre de “Historial4” la cual contenga los siguientes valores:
+
+#           7510, 7960, 76180, 800, 4100
+
+# Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Olivia”.
+#  Cree una función para determinar el valor mínimo de atención gastada en “Olivia”  mostrándolo en pantalla.
+
+historial4 = 7510, 7960, 76180, 800, 4100
+lista = list(historial4)
+
+print("El valor mínimo de que se gasto en una consulta fue: $ " + str(min(lista)))
+
+
+
+
+
+
