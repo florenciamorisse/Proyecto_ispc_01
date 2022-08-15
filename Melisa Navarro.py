55b99a56fec1cb90250e38814613a8df84c2aa22
Melisa Navarro <106558899+Meliweb1089@users.noreply.github.com>
2022-08-14 23:13:28 -0300

    Add files via upload

---
+++ Melisa Navarro.py
@@ -0,0 +1,6 @@
+
+Dueno2={"DNI":23546987,"NOMBRE":"Maria", "APELLIDO":"Perez","TELEFONO":4789689,"DIRECCION":"Puerreydon 811"}
+del Dueno2["DNI"]
+del Dueno2["APELLIDO"]
+for n in range(0,4):
+    print (Dueno2)
